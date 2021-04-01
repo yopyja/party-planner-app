86c36d6d872a83d4af6f3de102321cdeaa35bb8a
Patrick Jordan <ptj.bodega@gmail.com>
2021-04-01 15:34:27 -0500

    Update favicon, Add Login and Guest mockup w/ new navbar mockup, update router, modify HelloWorld prep for deletion

---
+++ src/components/Calendar.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="calendar-day block">
+    <div class="arrow-btn-container">
+      <a class="arrow-btn left" href="#202"
+        ><span class="icon fontawesome-angle-left"></span
+      ></a>
+      <h2 class="titular">APRIL 2013</h2>
+      <a class="arrow-btn right" href="#203"
+        ><span class="icon fontawesome-angle-right"></span
+      ></a>
+    </div>
+    <table class="calendar">
+      <thead class="days-week">
+        <tr>
+          <th>S</th>
+          <th>M</th>
+          <th>T</th>
+          <th>W</th>
+          <th>R</th>
+          <th>F</th>
+          <th>S</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><a class="scnd-font-color" href="#100">1</a></td>
+        </tr>
+        <tr>
+          <td><a class="scnd-font-color" href="#101">2</a></td>
+          <td><a class="scnd-font-color" href="#102">3</a></td>
+          <td><a class="scnd-font-color" href="#103">4</a></td>
+          <td><a class="scnd-font-color" href="#104">5</a></td>
+          <td><a class="scnd-font-color" href="#105">6</a></td>
+          <td><a class="scnd-font-color" href="#106">7</a></td>
+          <td><a class="scnd-font-color" href="#107">8</a></td>
+        </tr>
+        <tr>
+          <td><a class="scnd-font-color" href="#108">9</a></td>
+          <td><a class="scnd-font-color" href="#109">10</a></td>
+          <td><a class="scnd-font-color" href="#110">11</a></td>
+          <td><a class="scnd-font-color" href="#111">12</a></td>
+          <td><a class="scnd-font-color" href="#112">13</a></td>
+          <td><a class="scnd-font-color" href="#113">14</a></td>
+          <td><a class="scnd-font-color" href="#114">15</a></td>
+        </tr>
+        <tr>
+          <td><a class="scnd-font-color" href="#115">16</a></td>
+          <td><a class="scnd-font-color" href="#116">17</a></td>
+          <td><a class="scnd-font-color" href="#117">18</a></td>
+          <td><a class="scnd-font-color" href="#118">19</a></td>
+          <td><a class="scnd-font-color" href="#119">20</a></td>
+          <td><a class="scnd-font-color" href="#120">21</a></td>
+          <td><a class="scnd-font-color" href="#121">22</a></td>
+        </tr>
+        <tr>
+          <td><a class="scnd-font-color" href="#122">23</a></td>
+          <td><a class="scnd-font-color" href="#123">24</a></td>
+          <td><a class="scnd-font-color" href="#124">25</a></td>
+          <td><a class="today" href="#125">26</a></td>
+          <td><a href="#126">27</a></td>
+          <td><a href="#127">28</a></td>
+          <td><a href="#128">29</a></td>
+        </tr>
+        <tr>
+          <td><a href="#129">30</a></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <!-- end calendar-month block -->
+</template>
+
+<script>
+export default {
+  name: "Calendar",
+};
+</script>
+
+<style lang="scss">
+@import "../assets/sass/main";
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template lang="pug">    
+body 
+    div(class="login-container")
+        div(class="login-panel block") 
+
+            // login header 
+            h2(class="login-header titular") WELCOME 
+
+            // login body 
+            div(class="login-body") 
+
+                // email input 
+                div(class="input-container") 
+                    input(type="email" placeholder="email" class="email text-input")
+                    div(class="input-icon envelope-icon-acount") 
+                        span(class="fontawesome-envelope scnd-font-color")
+
+                // password input 
+                div(class="input-container")
+                    input(type="password" placeholder="password" class="password text-input")
+                    div(class="input-icon password-icon") 
+                        span(class="fontawesome-lock scnd-font-color")
+                        
+            // login footer 
+            div(class="login-footer") 
+                a(class="sign-in button", href="../") SIGN IN
+</template>
