36bfeb7ac5bbf7fffdcaef560ca1d4a2ee786750
Patrick Jordan <ptj.bodega@gmail.com>
2021-04-09 14:55:40 -0500

    created basic modal

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
     <Navigation />
+       <button
+      type="button"
+      class="btn"
+      @click="showModal"
+    >
+      Open Modal!
+    </button>
+
+    <Modal
+      v-show="isModalVisible"
+      @close="closeModal"
+    />
     <router-view />
   </div>
 </template>
@@ -11,9 +23,26 @@
 
 <script lang="ts">
 import Navigation from "./components/Navigation.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "app",
-  components: { Navigation },
+  components: {
+    Modal,
+    Navigation,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
 };
 </script>
